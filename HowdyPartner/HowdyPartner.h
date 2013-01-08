241fa75c36bfc35d89d9c127464313f94676db63
MikhailBasbas <mikhail.basbas@gmail.com>
2013-01-08 10:14:19 +0800

    Added HowdyPartner Class

---
+++ HowdyPartner/HowdyPartner.h
@@ -0,0 +1,13 @@
+//
+//  HowdyPartner.h
+//  HowdyPartner
+//
+//  Created by Mikhail Lomibao Basbas on 1/8/13.
+//  Copyright (c) 2013 Mikhail Lomibao Basbas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HowdyPartner : NSObject
+
+@end
