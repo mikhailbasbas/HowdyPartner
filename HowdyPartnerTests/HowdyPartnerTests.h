863617defc6735c8d781af938f50a45f4725efa8
MikhailBasbas <mikhail.basbas@gmail.com>
2013-01-08 09:58:25 +0800

    First Commit

---
+++ HowdyPartnerTests/HowdyPartnerTests.h
@@ -0,0 +1,13 @@
+//
+//  HowdyPartnerTests.h
+//  HowdyPartnerTests
+//
+//  Created by Mikhail Lomibao Basbas on 1/8/13.
+//  Copyright (c) 2013 Mikhail Lomibao Basbas. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HowdyPartnerTests : SenTestCase
+
+@end
---
+++ HowdyPartnerTests/HowdyPartnerTests.m
@@ -0,0 +1,32 @@
+//
+//  HowdyPartnerTests.m
+//  HowdyPartnerTests
+//
+//  Created by Mikhail Lomibao Basbas on 1/8/13.
+//  Copyright (c) 2013 Mikhail Lomibao Basbas. All rights reserved.
+//
+
+#import "HowdyPartnerTests.h"
+
+@implementation HowdyPartnerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HowdyPartnerTests");
+}
+
+@end
