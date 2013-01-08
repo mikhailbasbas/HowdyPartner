8b824a1d442ff5703ceb69b1b67994188e6f95a7
MikhailBasbas <mikhail.basbas@gmail.com>
2013-01-08 10:29:18 +0800

    Howdy Partner Added again test

---
+++ HowdyPartner.m
@@ -0,0 +1,13 @@
+//
+//  HowdyPartner.m
+//  HowdyPartner
+//
+//  Created by Mikhail Lomibao Basbas on 1/8/13.
+//  Copyright (c) 2013 Mikhail Lomibao Basbas. All rights reserved.
+//
+
+#import "HowdyPartner.h"
+
+@implementation HowdyPartner
+
+@end
